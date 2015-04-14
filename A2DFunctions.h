f7ed60d75685b4681ed47234f9f7f6a52042aa66
ashley <atowne@nd.edu>
2015-04-13 23:00:54 -0400

    A2D: for final board design (draft 1)

--- A2DFunctions.h
+++ A2DFunctions.h
@@ -10,9 +10,11 @@ unsigned int conv2();
 
 /**********************************************************/
 // define functions
+// CHOSA: use pins 17 & 18 (AN6 & AN7)
 void init_ADC()
 {
-    AD1PCFG = 0;            //Set pins to analog input
+    AD1PCFG = 0;            //Set pins to analog input - DOUBLE CHECK PGD & PGC
+    //AD1PCFG = 0x00C0;     //Set AN6 & AN7 to analog - all others are digital, including PGD & PGC
     AD1CON1bits.ADON = 0;   //Turn off ADC
     AD1CON1bits.FORM = 0;   //Unsigned integer format
     AD1CON1bits.SSRC = 0;   //Convert when SAMP is cleared
@@ -27,9 +29,10 @@ void init_ADC()
 
 unsigned int conv()
 {
+    // A2D_F
     unsigned int output;
 	LATD = 1;
-    AD1CHSbits.CH0SA = 2;       //Use channel AN0 as input
+    AD1CHSbits.CH0SA = 6;       //Use channel AN0 as input
     AD1CON1bits.SAMP = 1;       //Enable sampling
     delay_us(1); //Delay
     LATD = 0;
@@ -42,8 +45,9 @@ unsigned int conv()
 
 unsigned int conv1()
 {
-    unsigned int output;
-    AD1CHSbits.CH0SA = 2;       //Use channel AN2 as input for Mux A
+    // A2D_F 
+    unsigned int output; 
+    AD1CHSbits.CH0SA = 6;       //Use channel AN2 as input for Mux A 
     AD1CON1bits.SAMP = 1;       //Enable sampling
     delay_us(1);                //Delay
     AD1CON1bits.SAMP = 0;       //Terminate sampling
@@ -56,8 +60,9 @@ unsigned int conv1()
 
 unsigned int conv2()
 {
+    // A2D_UF
     unsigned int output;
-    AD1CHSbits.CH0SA = 3;       //Use channel AN2 as input for Mux A
+    AD1CHSbits.CH0SA = 7;       //Use channel AN2 as input for Mux A
     AD1CON1bits.SAMP = 1;       //Enable sampling
     delay_us(1);                //Delay
     AD1CON1bits.SAMP = 0;       //Terminate sampling
